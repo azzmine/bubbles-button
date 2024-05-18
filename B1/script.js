bdf7214d0b9344ccf3a5abf78020517494def523
azzmine <153880441+azzmine@users.noreply.github.com>
2024-05-18 10:45:15 +0700

    Add files via upload

---
+++ B1/script.js
@@ -0,0 +1,10 @@
+function startAnimation(){
+    let appDiv = document .getElementById('app').classList;
+
+    appDiv.add('active');
+
+    setTimeout(function () {
+        appDiv.remove('active');
+    }, 1800);
+    
+}
